1bffbfb9c02fbbf628284d3a30362f08a52b9fb1
Eli Pagan <mbk8689@gmail.com>
2022-02-06 14:36:20 -0500

    get all posts route created and works

--- server.js
+++ server.js
@@ -32,6 +32,23 @@ function findById(id, postsArray) {
     return result;
   }
 
+function findByTag(tags, postArray) {
+    const result = postArray.filter(posts => posts.tags === tags)[0];
+    return result;
+}
+
+app.get('/api/posts', (req, res) => {
+    let postsResults = posts;
+    if(req.query) {
+        postsResults = filterByQueryPosts(req.query, postsResults);
+    }
+    res.json(postsResults);
+});
+
+app.get('/api/posts/:id', (req, res) => {
+    const idResults
+})
+
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
 });
